7bbc1de1d6ca479709c28390af39bf29eaed1d5c
Shobhit <shobhitsingh1149201@gmail.com>
2024-05-23 00:34:39 +0530

    Initial commit with initial camera self check permission code

---
+++ app/src/main/java/com/example/cmrxmedium_f/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.cmrxmedium_f
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import com.example.cmrxmedium_f.ui.theme.CmrXMediumFTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val cameraPermissionRequest =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                // TODO() = Implement Camera related code
+            } else {
+                // TODO() = Camera Permission denied
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        when(PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) -> {
+                // TODO() = Camera Permission granted
+                // TODO() = Implement Camera related code
+            }
+            else -> {
+                cameraPermissionRequest.launch(android.Manifest.permission.CAMERA)
+            }
+
+        }
+
+
+
+        setContent {
+            CmrXMediumFTheme {
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CmrXMediumFTheme {
+
+    }
+}
